93c1503e362e6e8610371501ae0caecaddb60bd5
rajatphoneix <rajatphoenix@gmail.com>
2025-06-20 16:03:44 +0530

    project directories

--- setup/run-jenkins.sh
+++ setup/run-jenkins.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 #docker build -t custom-jenkins -f Dockerfile.jenkins .
-docker run -d --name jenkins   -p 8080:8080 -p 50000:50000   -v jenkins_home:/var/jenkins_home   -v /var/run/docker.sock:/var/run/docker.sock   custom-jenkins
+docker run -d --name jenkins \
+  -p 8080:8080 -p 50000:50000 \
+  -v jenkins_home:/var/jenkins_home \
+  -v /var/run/docker.sock:/var/run/docker.sock \
+  -v $HOME/.kube:/var/jenkins_home/.kube \
+  custom-jenkins
